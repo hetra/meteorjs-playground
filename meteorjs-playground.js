645669ef9bdb9637a2c84894210f308bc07eea47
Jack McPherson <hetra@users.noreply.github.com>
2015-04-11 17:35:30 +1000

    Added song descriptions and also the Skeleton CSS framework to make it look pretty.

--- meteorjs-playground.js
+++ meteorjs-playground.js
@@ -11,14 +11,17 @@ if (Meteor.isClient) {
     "submit .new-song": function(event){
       // create new song
 
+	  // map form fields to local variables
       var title = event.target.title.value;
       var url = event.target.url.value;
+	  var description = event.target.description.value;
 
-      Meteor.call("addSong", title, url);
+      Meteor.call("addSong", title, url); // add the song
 
       // clear the form
       event.target.title.value = "";
       event.target.url.value = "";
+      event.target.description.value = "";
 
       // prevent default form submission
       return false;
@@ -32,14 +35,16 @@ Accounts.ui.config({
 }
 
 Meteor.methods({
-  addSong: function(title, url){
+  addSong: function(title, url, description){ // the song's title, url, and description
     if(!Meteor.userId()){
       throw new Meteor.Error("not-authorized");
     }
 
+  // insert into db
   Songs.insert({
     title: title,
-    url: url
+    url: url,
+	description: description
   });
 
   }
